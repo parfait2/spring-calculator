3bc2c89718c2206c815b5662d8e78a4e6e851022
parfait2 <parkyl0208@naver.com>
2023-10-10 23:03:34 +0900

    [Add] MarketApi

---
+++ src/main/java/com/example/springcalculator/component/MarketApi.java
@@ -0,0 +1,8 @@
+package com.example.springcalculator.component;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class MarketApi {
+    public int connect() { return 1100; }
+}
